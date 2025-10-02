522f8f32b9a65e0fb0b3bfb66853ec08e71d9aee
Luciano Alberto Rodriguez <luciano.rodriguez@ar.ibm.com>
2025-10-02 13:44:32 -0300

    test unitario de ejemplo

--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from server import app
+import json
 
 client =  TestClient(
     app,
@@ -21,10 +22,12 @@ def test_read_main():
         "limit":"10",
         "page":"0"
     }
-    response = client.post("/monday/boards/list", json_post)
+    response = client.post(url="/monday/boards/list", content= json.dumps(json_post))
+    response_obj = json.load(response)
 
     # 2. Assert the expected HTTP status code
     assert response.status_code == 200
 
     # 3. Assert the expected JSON response body
-    assert response.json() == {"msg": "Hello World"}
+    assert response_obj["status"] == "success"
+    assert str(response_obj["response"][0]["message"]).startswith("Tableros disponibles en Monday.com:")
