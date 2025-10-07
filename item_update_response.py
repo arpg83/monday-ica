61ba65f0538f66b92a4200e43de9920435cbdef7
emabelpaco <45299597+emabelpaco@users.noreply.github.com>
2025-10-07 16:54:30 -0300

    template jinja get_item_updates

---
+++ item_update_response.py
@@ -0,0 +1,12 @@
+""" Clase para usar en template de item update"""
+class ItemUpdate:
+    """Clase para listar actualizaciones de Item de monday"""
+    id:str
+    content:str
+    created_at:str
+    updated_at:str
+    creator:str
+
+    def __init__(self):
+        pass
+
